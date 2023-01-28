6d18db4d1056bec421c3b052dd63e7f6327cc2d1
Mauritz Marais <mauritz303@gmail.com>
2023-01-28 16:05:30 +0200

    call displayResult function in fetch request

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,10 @@
-
+import { displayResult } from "./dom";
 // make fetch request to get shortened link
 export function getShortLink(request) {
     fetch(request)
         .then((response) => response.json())
-        // display results using json data*
-        .then((json) => console.log(json))
+        // display results using json data
+        .then((json) => displayResult(json))
         .catch((error) => console.error(`Error fetching data: ${error.message}`));
 }
 
