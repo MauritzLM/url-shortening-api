259b7b501d6e86c2136777d755836d1a97eccacf
Mauritz Marais <mauritz303@gmail.com>
2023-01-28 16:04:04 +0200

    updated displayResult function

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,42 @@
 // create shortened link ui
+function createLinkDiv(link, short) {
+    // containing div
+    const div = document.createElement("div");
+
+    // original link
+    const orignaLink = document.createElement("p");
+    orignaLink.textContent = link;
+
+    // short link div
+    const shortLinkDiv = document.createElement("div")
+    // short link 
+    const shortLink = document.createElement("p");
+    shortLink.textContent = short;
+
+    // button
+    const copyButton = document.createElement("button");
+    copyButton.textContent = "copy";
+    copyButton.classList.add("copy");
+
+    shortLinkDiv.append(shortLink, copyButton);
+    div.append(orignaLink, shortLinkDiv)
+
+    return div;
+}
+
 // function to display shortened link using json
+export function displayResult(json) {
+    console.log(json);
+    // get original link
+    let link = json.result.original_link;
+    // get short link
+    let short = json.result.full_short_link;
+    // 
+    let div = createLinkDiv(link, short);
+
+    let linksDiv = document.querySelector(".links-list");
+
+    linksDiv.appendChild(div);
+}
+
 // mobile navbar
